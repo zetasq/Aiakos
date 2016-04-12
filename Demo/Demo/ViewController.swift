85d6134f813408a2f09b41a67fa071d2d00e631a
Zhu Shengqi <zetasq@gmail.com>
2016-04-12 19:52:29 +0800

    Add demo project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Zhu Shengqi on 4/11/16.
+//  Copyright © 2016 Zhu Shengqi. All rights reserved.
+//
+
+import UIKit
+import Aiakos
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
